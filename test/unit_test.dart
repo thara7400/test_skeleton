4b6931264caec461b6a8cac5fada97658dd9aca2
thara7400 <57885640+thara7400@users.noreply.github.com>
2022-08-31 19:10:19 +0900

    Extended example unit test

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,15 +1,20 @@
-// This is an example unit test.
-//
-// A unit test tests a single function, method, or class. To learn more about
-// writing unit tests, visit
-// https://flutter.dev/docs/cookbook/testing/unit/introduction
-
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('Plus Operator', () {
     test('should add two numbers together', () {
       expect(1 + 1, 2);
+    });
+    test('should add three numbers together', () {
+      expect(3 + 5 + 7, 15);
+    });
+    // test('[failure]should add three numbers together', () {
+    //   expect(3 + 5 + 7, 1);
+    // });
+  });
+  group('Minus Operator', () {
+    test('subtract two numbers', () {
+      expect(5 - 2, 3);
     });
   });
 }
